ed424808bbcdb03051a21dd9464dca042ec40260
andyafter <andyafter@gmail.com>
2017-04-24 22:44:37 +0800

    (Server) User schema.

--- plenti/index.js
+++ plenti/index.js
@@ -1,17 +1,33 @@
-var Hapi = require('hapi');
+'use strict';
+
+const Hapi = require('hapi');
+const Boom = require('boom');
+const mongoose = require('mongoose');
+const glob = require('glob');
+const path = require('path');
+const secret = require('./config');
 
 var server = new Hapi.Server();
+
 server.connection({
     host: 'localhost',
-    port: 1337
+    port: 5000
 });
 
-server.route({
-    method: 'GET',
-    path: '/hello',
-    handler: function(request, reply){
-        reply('Hello World');
-    }
+server.register(require('hapi-auth-jwt'), (err) => {
+
+    server.auth.strategy('jwt', 'jwt', {
+        key: secret,
+        verifyOptions: { algorithms: ['HS256'] }
+    });
+
+    // create a new route for all files in src/route
+    glob.sync('./src/routes/*.js', {
+        root: __dirname
+    }).forEach(file => {
+        const route = require(path.join(__dirname, file));
+        server.route(route);
+    });
 });
 
 server.start(function(){
