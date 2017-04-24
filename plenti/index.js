d432a6f29561292bd69b1952b29557411cec9f60
andyafter <andyafter@gmail.com>
2017-04-25 00:47:32 +0800

    (Server)User login and signup routes and schema.

--- plenti/index.js
+++ plenti/index.js
@@ -14,23 +14,7 @@ server.connection({
     port: 5000
 });
 
-const dbUrl = 'mongodb://localhost:27017/hapi-app';
 
-server.register(require('hapi-auth-jwt'), (err) => {
-
-    server.auth.strategy('jwt', 'jwt', {
-        key: secret,
-        verifyOptions: { algorithms: ['HS256'] }
-    });
-
-    // create a new route for all files in src/route
-    glob.sync('./src/*/routes/*.js', {
-        root: __dirname
-    }).forEach(file => {
-        const route = require(path.join(__dirname, file));
-        server.route(route);
-    });
-});
 
 server.start((err) => {
     if (err) {
