6360dd8622c631c9865a0173b1cf5d0399228edb
andyafter <andyafter@gmail.com>
2017-04-26 23:25:05 +0800

    (Web) User profile component

---
+++ angular/src/app/user/profile.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'app-user-profile',
+    template: './profile.tpl.html'
+})
+
+export class UserProfileComponent{
+    // here should be restful API to fetch and modify data
+    
+}
