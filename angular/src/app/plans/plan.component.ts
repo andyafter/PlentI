29d73f0faf727fea474cdabb9aa0ebdc55bc07e1
andyafter <andyafter@gmail.com>
2017-04-27 00:30:00 +0800

    (Web) Plan component

---
+++ angular/src/app/plans/plan.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'plan',
+    template: `
+        <h2> Plan Component! </h2>
+    `
+})
+
+export class PlanComponent {
+
+}
+
---
+++ angular/src/app/plans/planlist.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'plan-list',
+    template: `<h2>Plan List!!!!</h2>`
+})
+
+export class PlanListComponent {
+    // here choose the top 10 plans
+}
+
