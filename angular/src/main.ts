0bee1a37f9cbbd91872fbbebeca236f15e9e6d17
andyafter <andyafter@gmail.com>
2017-04-26 14:51:50 +0800

    (Development) Fixed gulp ~Duplicate identifier 'PropertyKey'~

--- angular/src/main.ts
+++ angular/src/main.ts
@@ -1,5 +1,3 @@
-/// <reference path="../../node_modules/typings/browser.d.ts" />
-
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
